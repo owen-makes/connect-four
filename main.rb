2de76189279212ac5b5933b39a76e81fc3038083
owen-makes <owen.botto@gmail.com>
2024-07-23 23:44:21 -0300

    Add most game functionalities and corresponding tests

--- main.rb
+++ main.rb
@@ -1,7 +1,103 @@
 class ConnectFour
-  attr_reader :board
+  attr_reader :board, :board_ui, :player1, :player2
   
   def initialize
-    @board = []
+    @board = Array.new(7) {Array.new(6,"⭕")}
+    @board_ui = pretty_print
+    @player1 = '🟡'
+    @player2 = '🔵'
+  end
+
+  def pretty_print
+    "|0 |1 |2 |3 |4 |5 |6 |\n"\
+      "|__|#{"__|"*6}\n"\
+      "|#{@board[0][0]}|#{@board[1][0]}|#{@board[2][0]}|#{@board[3][0]}|#{@board[4][0]}|#{@board[5][0]}|#{@board[6][0]}|\n"\
+      "|#{@board[0][1]}|#{@board[1][1]}|#{@board[2][1]}|#{@board[3][1]}|#{@board[4][1]}|#{@board[5][1]}|#{@board[6][1]}|\n"\
+      "|#{@board[0][2]}|#{@board[1][2]}|#{@board[2][2]}|#{@board[3][2]}|#{@board[4][2]}|#{@board[5][2]}|#{@board[6][2]}|\n"\
+      "|#{@board[0][3]}|#{@board[1][3]}|#{@board[2][3]}|#{@board[3][3]}|#{@board[4][3]}|#{@board[5][3]}|#{@board[6][3]}|\n"\
+      "|#{@board[0][4]}|#{@board[1][4]}|#{@board[2][0]}|#{@board[3][4]}|#{@board[4][0]}|#{@board[5][4]}|#{@board[6][0]}|\n"\
+      "|#{@board[0][5]}|#{@board[1][5]}|#{@board[2][5]}|#{@board[3][5]}|#{@board[4][5]}|#{@board[5][5]}|#{@board[6][5]}|\n"\
+      "#{"⁻"*22}"
+  end
+
+  def turn(player)
+    puts "Player(#{player}) select a column:"
+    input = gets.chomp.to_i
+    until verify_input(input)
+      puts "Player(#{player}) select a column:"
+      input = gets.chomp.to_i
+    end
+    make_move(player, input)
+  end
+
+  def make_move(player, column)
+    a = @board[column]
+    if a.all?("⭕")
+      @board[column][-1] = player
+    else
+      @board[column][a.rindex("⭕")] = player
+    end
+  end
+
+  def verify_input(user_input)
+    begin
+      if user_input > 6 || @board[user_input].none?("⭕")
+        false
+      else
+        true
+      end
+    rescue
+      false
+    end
+  end
+
+  def game_over?
+    win?(@player1) || win?(@player2) || board_full?
+  end
+
+  def win?(player)
+    win_combinations.any? do |combination|
+      combination.all? { |row, col| @board[col][row] == player }
+    end
+  end
+
+  def board_full?
+    @board.all? { |column| column.none? { |cell| cell == "⭕" } }
+  end
+  
+  def win_combinations
+    horizontal_wins + vertical_wins + diagonal_wins_up + diagonal_wins_down
+  end
+  
+  def horizontal_wins
+    (0..5).flat_map do |row|
+      (0..3).map do |col|
+        [[row, col], [row, col+1], [row, col+2], [row, col+3]]
+      end
+    end
+  end
+  
+  def vertical_wins
+    (0..6).flat_map do |col|
+      (0..2).map do |row|
+        [[row, col], [row+1, col], [row+2, col], [row+3, col]]
+      end
+    end
+  end
+  
+  def diagonal_wins_up
+    (0..2).flat_map do |row|
+      (0..3).map do |col|
+        [[row, col], [row+1, col+1], [row+2, col+2], [row+3, col+3]]
+      end
+    end
+  end
+  
+  def diagonal_wins_down
+    (3..5).flat_map do |row|
+      (0..3).map do |col|
+        [[row, col], [row-1, col+1], [row-2, col+2], [row-3, col+3]]
+      end
+    end
   end
 end
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -4,9 +4,171 @@ describe ConnectFour do
   subject(:game) {described_class.new}
   
   describe '#initialize' do
-    it 'prints board when initialized' do
+    it 'generates board as nested array' do
       board = game.board
-      expect(board).to_not be_nil    
+      expect(board).to be_an_instance_of(Array)    
+    end
+
+    it 'generates board_ui' do
+      board_ui = game.board_ui
+      expect(board_ui).to be_an_instance_of(String)
+    end
+  end
+
+
+  describe '#pretty_print' do
+    it 'returns board ui' do
+      board = game.board
+      message = "|0 |1 |2 |3 |4 |5 |6 |\n"\
+      "|__|#{"__|"*6}\n"\
+      "|#{board[0][0]}|#{board[1][0]}|#{board[2][0]}|#{board[3][0]}|#{board[4][0]}|#{board[5][0]}|#{board[6][0]}|\n"\
+      "|#{board[0][1]}|#{board[1][1]}|#{board[2][1]}|#{board[3][1]}|#{board[4][1]}|#{board[5][1]}|#{board[6][1]}|\n"\
+      "|#{board[0][2]}|#{board[1][2]}|#{board[2][2]}|#{board[3][2]}|#{board[4][2]}|#{board[5][2]}|#{board[6][2]}|\n"\
+      "|#{board[0][3]}|#{board[1][3]}|#{board[2][3]}|#{board[3][3]}|#{board[4][3]}|#{board[5][3]}|#{board[6][3]}|\n"\
+      "|#{board[0][4]}|#{board[1][4]}|#{board[2][0]}|#{board[3][4]}|#{board[4][0]}|#{board[5][4]}|#{board[6][0]}|\n"\
+      "|#{board[0][5]}|#{board[1][5]}|#{board[2][5]}|#{board[3][5]}|#{board[4][5]}|#{board[5][5]}|#{board[6][5]}|\n"\
+      "#{"⁻"*22}"
+      expect(game.pretty_print).to eq(message)
+    end
+  end
+
+  describe '#play' do
+    xit 'loops' do
+      #continue here
+    end
+  end
+
+  describe '#make_move' do
+    it 'updates board' do
+      player = game.player2
+      column = 0
+      expect{game.make_move(player, column)}.to change { game.board[0][5] }.from("⭕").to(player)
+    end
+  end
+
+  describe '#game_over?' do
+    it 'returns true when game won' do
+      player = game.player2
+      game.make_move(player, 0)
+      game.make_move(player, 0)
+      game.make_move(player, 0)
+      game.make_move(player, 0)
+      expect(game.game_over?).to eq(true)
+    end
+
+    it 'returns false when game is ongoing' do
+      player1 = game.player1
+      player2 = game.player2
+      game.make_move(player1, 0)
+      game.make_move(player1, 0)
+      game.make_move(player2, 0)
+      game.make_move(player2, 0)
+      expect(game.game_over?).to eq(false)
+    end
+  end
+
+  describe "#win?" do
+    it 'returns true when player 1 has won' do
+      player = game.player1
+      game.make_move(player, 0)
+      game.make_move(player, 0)
+      game.make_move(player, 0)
+      game.make_move(player, 0)
+      expect(game.win?(player)).to eq(true)
+    end
+
+    it 'returns true when player 2 has won' do
+      player = game.player2
+      game.make_move(player, 0)
+      game.make_move(player, 0)
+      game.make_move(player, 0)
+      game.make_move(player, 0)
+      expect(game.win?(player)).to eq(true)
+    end
+
+    it 'returns false when no player won' do
+      player1 = game.player1
+      player2 = game.player2
+      game.board[0].shift(4)
+      game.board[0].push(player1, player2, player1, player2)
+      expect(game.win?(player2)).to eq(false)
+      expect(game.win?(player1)).to eq(false)
+    end
+  end
+
+  describe "#board_full?" do
+    it 'returns true when board is full' do
+      player = game.player2
+      game.board.map { |column| column.fill(player) }
+      expect(game.board_full?).to eq(true)
+    end
+
+    it 'returns false when board not full' do
+      expect(game.board_full?).to eq(false)
+    end
+  end
+
+  describe '#turn' do
+
+    it 'prints message asking for input' do
+      player = game.player1
+      message = "Player(#{player}) select a column:\n"
+      expect{game.turn(player)}.to output(message).to_stdout
+    end
+
+    context 'when user inputs one incorrect value, then a valid input' do
+      before do
+        invalid_input = "10\n"
+        valid_input = "3\n"
+        allow(game).to receive(:gets).and_return(invalid_input, valid_input)
+      end
+
+      it 'breaks loop if input is valid' do
+        player = game.player1
+        msg = "Player(#{player}) select a column:"
+        expect(game).to receive(:puts).with(msg).twice
+        expect(game).to receive(:make_move).with(player,3)
+        game.turn(player)
+      end
+     end
+
+    it 'calls make_move' do
+      player = game.player1
+      allow(game).to receive(:gets).and_return("5\n")
+      allow(game).to receive(:verify_input).and_return(true)
+      expect(game).to receive(:make_move).with(player, 5)
+      game.turn(player)
+    end
+
+  end
+
+  describe '#verify_input' do
+    context 'when user selects a column out of bounds' do
+      user_input = 8
+      it 'returns false' do
+        expect(game.verify_input(user_input)).to eq(false)
+      end
+    end
+    context 'when selected column is full' do
+      game = described_class.new
+      game.board[0].fill('🟡')
+      user_input = 0
+
+      it 'returns false' do
+        expect(game.verify_input(user_input)).to eq(false)
+      end
+    end
+
+    context 'when input a string' do
+      user_input = 'F'
+      it 'returns false' do
+        expect(game.verify_input(user_input)).to eq(false)
+      end
+    end
+
+    it 'returns true when input is valid' do
+      user_input = 1
+      expect(game.verify_input(user_input)).to eq(true)
     end
   end
 end
